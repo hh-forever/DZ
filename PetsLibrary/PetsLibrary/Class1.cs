13f84a7b2bef2d7f1be449d18a483bed96028cd0
hh-forever <71729607+hh-forever@users.noreply.github.com>
2020-12-04 12:56:06 +0400

    Add files via upload

---
+++ PetsLibrary/PetsLibrary/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsLibrary
+{
+    public abstract class Cat
+
+    {
+
+        public string Name;
+
+        public byte Age;
+
+        public byte Weight;
+
+        public abstract string Meow();
+
+        public Cat()
+
+        {
+
+        }
+
+        public Cat(string n, byte age, byte w)
+
+        {
+
+            Name = n;
+
+            Age = age;
+
+            Weight = w;
+
+        }
+
+    }
+
+}
