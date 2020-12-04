111abfd6e0a082b7206b76ea9d15bac9790889e1
hh-forever <71729607+hh-forever@users.noreply.github.com>
2020-12-04 13:02:21 +0400

    Add files via upload

---
+++ PetsClient/PetsClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using PetsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCat Bagira = new MyCat();
+            Bagira.Meow(); // Метод Meow() из DLL
+            Console.ReadLine();
+        }
+    }
+}
